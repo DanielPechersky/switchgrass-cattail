62fb583936924ceca66e4d455b5e6c7ced0efe3e
Daniel Pechersky <danny.pechersky@gmail.com>
2025-11-09 13:23:17 -0500

    Initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::timg::TimerGroup;
+
+use defmt::info;
+use esp_println as _;
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+
+use esp_backtrace as _;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[esp_rtos::main]
+async fn main(_spawner: Spawner) -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
+
+    info!("Embassy initialized!");
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
+}
