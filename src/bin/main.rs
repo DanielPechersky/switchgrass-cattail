3a9ffedb9cdc3e5f098c0c2942a146b63856a2ca
Daniel Pechersky <danny.pechersky@gmail.com>
2025-11-25 20:54:17 -0500

    even more sensitive

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -95,7 +95,7 @@ async fn handle_mpu6050(
         info!("sending {}", msg.as_str());
         out.write_async(msg.as_bytes()).await.unwrap();
 
-        let min_value = 0.9;
+        let min_value = 0.93;
         let max_value = 0.95;
         let uprightness = (-acc.x()).max(0.0);
         let uprightness =
